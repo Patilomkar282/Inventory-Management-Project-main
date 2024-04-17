eebc6ccb03e068292763c7d1b66a218958820aa2
teju1604 <tejumulinti@gmail.com>
2024-04-17 20:10:43 +0530

    applied email validation..
    applied email validation..

--- login.php
+++ login.php
@@ -1,11 +1,18 @@
 <?php
+
 session_start();
 include_once('connection.php');
 
 if (isset($_POST['login'])) {
-
     $username = $_POST['username'];
     $password = md5($_POST['password']);
+
+    
+    $email_pattern = "/^[a-zA-Z0-9._%+-]+@mmcoe\.edu\.in$/"; 
+    if (!filter_var($username, FILTER_VALIDATE_EMAIL) || !preg_match($email_pattern, $username)) {
+        echo "<script>alert('Invalid Email Format or Pattern');</script>";
+        exit;
+    }
 
     $sql = "SELECT * FROM `tbl_user` WHERE `username`='$username' AND `password`='$password'";
     $result = mysqli_query($conn, $sql);
@@ -26,7 +33,6 @@ if (isset($_POST['login'])) {
             $username = $row['username'];
             $password = $row['password'];
 
-
             if ($username == $username && $password == $password) {
                 $_SESSION['name'] = $name;
                 $_SESSION['username'] = $username;
@@ -38,6 +44,6 @@ if (isset($_POST['login'])) {
             exit;
         }
     }
-
 }
+
 ?>
